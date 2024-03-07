43231414cc01291f85166805f3cd52e16ce3b546
Ben Plunkert <ben@plunkert.com>
2024-03-07 16:00:51 -0500

    update tests

--- internal/parser/tests/missing_file_path_test.go
+++ internal/parser/tests/missing_file_path_test.go
@@ -2,6 +2,7 @@ package parser_test
 
 import (
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/inferret/infer/internal/parser"
@@ -11,20 +12,25 @@ func TestParseInferfileMissingFilePath(t *testing.T) {
 	testCases := []struct {
 		name          string
 		inferfilePath string
-		expectedErr   string
+		expectedError string
 	}{
 		{
 			name:          "missing_file_path",
 			inferfilePath: filepath.Join("../testdata", "missing_file_path.hcl"),
-			expectedErr:   "file not found: /this/file/does/not/exist (Inferfile line: 1)",
+			expectedError: `failed to parse Inferfile:
+- ../testdata/missing_file_path.hcl:2,45-45: Missing required argument; The argument "code" is required, but no definition was found.
+- ../testdata/missing_file_path.hcl:3,5-10: Unsupported block type; Blocks of type "infer" are not expected here.`,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			_, err := parser.ParseInferfile(tc.inferfilePath)
-			if err == nil || err.Error() != tc.expectedErr {
-				t.Errorf("Expected error: %s, got: %v", tc.expectedErr, err)
+			if err == nil {
+				t.Fatal("Expected an error, but got nil")
+			}
+			if strings.TrimSpace(err.Error()) != strings.TrimSpace(tc.expectedError) {
+				t.Errorf("Expected error:\n%s\ngot:\n%s", tc.expectedError, err.Error())
 			}
 		})
 	}
